eac9665c24abc660fb77a5bc40965ff03dae0055
AtomMushroom <vasilyev.atom@gmail.com>
2023-05-05 23:21:56 +0300

    слава богу не пизданулось

--- Temperature/app/src/main/java/com/luk/temperature/MainActivity.java
+++ Temperature/app/src/main/java/com/luk/temperature/MainActivity.java
@@ -8,17 +8,24 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import android.os.CountDownTimer;
-import android.util.Log;
 import android.widget.TextView;
-import com.luk.temperature.CpuFreq;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.Viewport;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     private TextView textView, level, voltage, batteryType, temperature, cpu;
+    private static final Random RANDOM = new Random();
     BroadcastReceiver batteryBroadcast;
     IntentFilter intentFilter;
-    CpuFreq cpuFreq;
+    GraphView graphView;
+    LineGraphSeries<DataPoint> series;
+    int x;
+    private int lastX = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +38,19 @@ public class MainActivity extends AppCompatActivity {
         batteryType = findViewById(R.id.batteryType);
         temperature = findViewById(R.id.temperature);
         cpu = findViewById(R.id.cpu);
+        graphView = findViewById(R.id.graph1);
+        x = 1;
+        series = new LineGraphSeries<DataPoint>();
+        graphView.addSeries(series);
+        // customize a little bit viewport
+        Viewport viewport = graphView.getViewport();
+        viewport.setYAxisBoundsManual(true);
+        viewport.setXAxisBoundsManual(true);
+        viewport.setMinY(0);
+        viewport.setMaxX(15);
+        viewport.setMaxY(50);
+        viewport.setScrollable(true);
+        series.setTitle("Изменения температуры процессора");
 
         intentFilterAndBroadcast();
     }
@@ -60,9 +80,10 @@ public class MainActivity extends AppCompatActivity {
             public void onTick(long millisUntilFinished) {}
             @Override
             public void onFinish() {
-                double i1 = 0.3 + r.nextDouble() * (6.5 - 0.3);
+                double i1 = 0.3 + r.nextDouble() * (3.0 - 0.5);
                 String s = String.format("%.2f", tempTemp+i1);
-                cpu.setText("Температура процессора: " + (s) + "C");
+                cpu.setText("Температура процессора: " + (s) + "°C");
+                series.appendData(new DataPoint(lastX++, tempTemp+i1), true, 25);
                 start();
             }
         }.start();
@@ -78,7 +99,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         unregisterReceiver(batteryBroadcast);
-    }
+    }/*
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // we're going to simulate real time with thread that append data to the graph
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                // we add 100 new entries
+                for (int i = 0; i < 100; i++) {
+                    runOnUiThread(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            addEntry();
+                        }
+                    });
+
+                    // sleep to slow down the add of entries
+                    try {
+                        Thread.sleep(600);
+                    } catch (InterruptedException e) {
+                        // manage error ...
+                    }
+                }
+            }
+        }).start();
+    }*/
     private String getCpuUsageFreq() {
         String info = new String();
         info += "using getCoresUsageGuessFromFreq";
@@ -97,5 +146,9 @@ public class MainActivity extends AppCompatActivity {
         info += "  moy=" + cores[0] + "% \n";
         info += "CPU total: " + CpuFreq.getCpuUsage(cores) + "%";
         return info;
+    }
+    private void addEntry() {
+        // here, we choose to display max 10 points on the viewport and we scroll to end
+        //series.appendData(new DataPoint(lastX++, RANDOM.nextDouble() * 10d), true, 10);
     }
 }
